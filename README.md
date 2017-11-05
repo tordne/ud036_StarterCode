5488f1ea4b5734947c2d291610d7aeebe2b55ba0
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2017-11-05 20:25:16 +0000

    V 0.0.4 * Added comments to the movie instances in entertainment_center.py * Remodelled the README.md and added more information about the installation and usage.

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
 # ud036_StarterCode -- Movie Trailer
 
 ## Synopsis
-This is the source code for a website showing movie trailers
+This Movie Trailer project is the first project of the Full Stack Web Develper course of Udacity.
+This project cotaines the source code for a website showing movie trailers.
 
-## Preerquisites
+## Prerequisites
 This project was written in Python 2.7.14 and only uses PSL.
+### Linux Instructions
+Make sure you have installed python 2.7.X
+Open a terminal commandline and enter:
+`python -V`
+else follow your distribution instruction to install python 2.7.X
 
-## Running the site
+## How to install
+### Linux Instructions
+Open a terminal command line and enter:
+`git clone https://github.com/tordne/ud036_StarterCode.git`
+
+## How to run the site
+### Linux Instructions
+Open a terminal command line and go to the projects directory:
+`cd /home/XXX/ud036_StarterCode`
+then to run the website:
+`python entertainment_center.py`
+This will open a new tab in your webbrowser which will be the website.
+
+## Project contents
+### media.py
+This file contains th Movie class, which will need several attributes
+* Movie Title
+* Movie Poster Link
+* Movie Trailer Link
+
+There is also one method which will open the link of the movie trailer in a webbrowser.
+`Movie.show_trailer()`
+
+### entertainment_center.py
+This file contains all the Movie Instances, please feel free to remove and add your movies by using the movie class with the following syntax:
 ```
-python entertainment_center.py
+# Khan Kluay movie: movie title, poster image, movie trailer
+kluay = media.Movie(
+    "Khan Kluay", 
+    "https://upload.wikimedia.org/wikipedia/en/7/77/Khankluay.jpg", 
+    "https://www.youtube.com/watch?v=DsmWluXpLQw"
+    )
 ```
+
+Within the **__main__** there is one list variable **movies** which will contain all the movies that will be used on the HTML page.
--- entertainment_center.py
+++ entertainment_center.py
@@ -2,35 +2,60 @@ import media
 import fresh_tomatoes
 
 
-kluay = media.Movie("Khan Kluay", 
+# Khan Kluay movie: movie title, poster image, movie trailer
+kluay = media.Movie(
+	"Khan Kluay", 
 	"https://upload.wikimedia.org/wikipedia/en/7/77/Khankluay.jpg", 
-	"https://www.youtube.com/watch?v=DsmWluXpLQw")
+	"https://www.youtube.com/watch?v=DsmWluXpLQw"
+	)
 
-kluay2 = media.Movie("Khan Kluay 2", 
+# Khan Kluay 2 movie: movie title, poster image, movie trailer
+kluay2 = media.Movie(
+	"Khan Kluay 2", 
 	"http://2.bp.blogspot.com/-bR_dJ4QJgOM/VDE_RLm56UI/AAAAAAAABNU/w0mbiGsr56w/s1600/khan%2Bkluay%2B2%2Bposter.jpg", 
-	"https://www.youtube.com/watch?v=crZ8xm0msH4")
+	"https://www.youtube.com/watch?v=crZ8xm0msH4"
+	)
 
-toy_story = media.Movie("Toy Story", 
+# Toy Story movie: movie title, poster image, movie trailer
+toy_story = media.Movie(
+	"Toy Story", 
 	"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+	"https://www.youtube.com/watch?v=KYz2wyBy3kc"
+	)
 
-avatar = media.Movie("Avatar", 
+# Avatar movie: movie title, poster image, movie trailer
+avatar = media.Movie(
+	"Avatar", 
 	"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-	"https://www.youtube.com/watch?v=d1_JBMrrYw8")
+	"https://www.youtube.com/watch?v=d1_JBMrrYw8"
+	)
 
-despicable = media.Movie("Despicable Me", 
+# Despicable Me movie: movie title, poster image, movie trailer
+despicable = media.Movie(
+	"Despicable Me", 
 	"https://upload.wikimedia.org/wikipedia/en/d/db/Despicable_Me_Poster.jpg",
-	"https://www.youtube.com/watch?v=TZkAcKCFIVo")
+	"https://www.youtube.com/watch?v=TZkAcKCFIVo"
+	)
 
-ice_age = media.Movie("Ice Age", 
+# Ice Age movie: movie title, poster image, movie trailer
+ice_age = media.Movie(
+	"Ice Age", 
 	"https://upload.wikimedia.org/wikipedia/en/3/3c/Ice_Age_%282002_film%29_poster.jpg", 
-	"https://www.youtube.com/watch?v=wjdqn9r4thg")
+	"https://www.youtube.com/watch?v=wjdqn9r4thg"
+	)
 
 
 if __name__ == '__main__':
 
-	# Create a list of the movies
-	movies = [kluay, kluay2, toy_story, avatar, despicable, ice_age]
+	# Create a list of the movies that will be passed to the movie webpage
+	movies = [
+		kluay, 
+		kluay2, 
+		toy_story, 
+		avatar, 
+		despicable, 
+		ice_age
+		]
 
 	# Open the fresh tomatoes movie webpage
 	fresh_tomatoes.open_movies_page(movies)
